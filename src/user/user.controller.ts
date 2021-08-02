a3ba47f66773357a86c0d3bdc6b3195c14129e4c
presetslrdev <presetslr@gmail.com>
2021-08-02 09:04:05 +0300

    add middleware, dto, validation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,43 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserResponseInterface } from '@app/user/types/userResponse.interface';
+import { LoginDto } from '@app/user/dto/login.dto';
+import { Request } from 'express';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
+  @UsePipes(new ValidationPipe())
   async createUser(
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<UserResponseInterface> {
     const user = await this.userService.createUser(createUserDto);
     return this.userService.buildUserResponse(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async login(
+    // Из запроса получаем Body.user
+    @Body('user') loginDto: LoginDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.login(loginDto);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Get('user')
+  async currentUser(@Req() request: Request): Promise<UserResponseInterface> {
+    return 'currentUser' as any;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserEntity } from '@app/user/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -6,6 +6,8 @@ import { Repository } from 'typeorm';
 import { sign } from 'jsonwebtoken';
 import { JWT_SECRET } from '@app/config';
 import { UserResponseInterface } from '@app/user/types/userResponse.interface';
+import { LoginDto } from '@app/user/dto/login.dto';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -15,9 +17,52 @@ export class UserService {
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    // Кастомная валидация
+    // есть ли в базе такой пользователь
+    const userByEmail = await this.userRepository.findOne({
+      email: createUserDto.email,
+    });
+    // есть ли в базе такой пользователь
+    const userByUsername = await this.userRepository.findOne({
+      username: createUserDto.username,
+    });
+    // Если есть - выкидываем ошибку
+    if (userByEmail || userByUsername) {
+      throw new HttpException(
+        'Email or username are taken',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
+  }
+
+  async login(loginDto: LoginDto): Promise<UserEntity> {
+    const user = await this.userRepository.findOne(
+      {
+        email: loginDto.email,
+      },
+      // Т.к. в user.entity заблочили password - делаем select всех данных заново
+      { select: ['id', 'username', 'email', 'bio', 'image', 'password'] },
+    );
+    if (!user) {
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    const isUserPasswordValid = await compare(loginDto.password, user.password);
+    if (!isUserPasswordValid) {
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    // Удаляем из user поле password
+    delete user.password;
+    return user;
   }
 
   generateJwt(user: UserEntity): string {
